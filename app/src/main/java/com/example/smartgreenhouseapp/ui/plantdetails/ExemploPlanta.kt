1c130304d2e83ddd9fd07d5f261c0fc3ae230e73
Walmircsanto <walmirsantosrosa@gmail.com>
2024-12-09 14:48:02 -0300

    :bug: fix: Corrigir a intenção de manda a imagem

--- app/src/main/java/com/example/smartgreenhouseapp/ui/plantdetails/ExemploPlanta.kt
+++ app/src/main/java/com/example/smartgreenhouseapp/ui/plantdetails/ExemploPlanta.kt
@@ -1,6 +1,7 @@
 package com.example.smartgreenhouseapp.ui.plantdetails
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +17,18 @@ class ExemploPlanta : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        setContentView(R.layout.activity_exemplo_planta)
+
+        val dynamicImageView: ImageView = findViewById(R.id.imageView2)
+
+        // Recebendo o ID da imagem passada pela Intent
+        val imageResId = intent.getIntExtra("IMAGE_ID", -1)
+
+        // Verificando se um ID válido foi recebido
+        if (imageResId != -1) {
+            dynamicImageView.setImageResource(imageResId)
         }
     }
 }
