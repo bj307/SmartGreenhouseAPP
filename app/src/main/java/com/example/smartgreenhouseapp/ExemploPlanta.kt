c15d129c349c0ed2341e4d248e434f640244ee19
Walmircsanto <walmirsantosrosa@gmail.com>
2024-12-09 10:31:02 -0300

    :sparkles: feat(exemplo_planta): Criar tela que apresenta as informações das plantas

---
+++ app/src/main/java/com/example/smartgreenhouseapp/ExemploPlanta.kt
@@ -0,0 +1,20 @@
+package com.example.smartgreenhouseapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ExemploPlanta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_exemplo_planta)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
